ef3cbb0cb137142f415a5686eb366f599a576af2
sinhlh <sinhlh@fpt.com>
2024-02-06 20:13:51 +0700

    Initial Commit

---
+++ MemoryLeak/MemoryLeakApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemoryLeakApp.swift
+//  MemoryLeak
+//
+//  Created by Sam on 06/02/2024.
+//
+
+import SwiftUI
+
+@main
+struct MemoryLeakApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
